2b99de5c13266c22597f0e595d00ce4703a31654
Cecil Urena <cecil.urena@gmail.com>
2023-09-03 17:28:27 -0500

    Initial Spring Boot app

---
+++ src/test/kotlin/org/cecil/commonawsjvm/CommonAwsJvmApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cecil.commonawsjvm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonAwsJvmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
